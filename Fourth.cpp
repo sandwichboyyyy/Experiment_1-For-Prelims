29ac3ad2d6b51ee4c5287109e33a1ece0723fd28
Nathaniel <nathaniel.masangkay.eng@ust.edu.ph>
2019-03-26 19:25:50 +0800

    Second Commit

---
+++ Fourth.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "static_cast<int> (7.9) = "
+    << static_cast<int> (7.9)
+    << endl;
+    cout << "static_cast<int> (3.3) = "
+    << static_cast<int> (3.3)
+    << endl;
+    cout << "static_cast<int> (25) = "
+    << static_cast<int> (25)
+    << endl;
+    cout << "static_cast<int> (7.8 + static_cast<double>(15/2)) = "
+    << static_cast<int> (7.8 + static_cast<double>(15/2))
+    << endl;
+    return 0;
+}
